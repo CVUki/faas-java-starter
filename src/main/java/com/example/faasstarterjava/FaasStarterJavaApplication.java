4a4ad7800bd315b88dcd4101df89ea39fb53cc8e
CVUki <CVE210557@spengergasse.at>
2025-12-28 01:04:11 +0100

    Initial Spring Boot project

---
+++ src/main/java/com/example/faasstarterjava/FaasStarterJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.faasstarterjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaasStarterJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaasStarterJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/faasstarterjava/FaasStarterJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.faasstarterjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaasStarterJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
